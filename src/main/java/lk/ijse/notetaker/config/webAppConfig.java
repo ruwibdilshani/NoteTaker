343b5187ca534e96d205b8690f8338ff15dd6752
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-08-25 16:08:24 +0530

    completed configs and created table

--- src/main/java/lk/ijse/notetaker/config/webAppConfig.java
+++ src/main/java/lk/ijse/notetaker/config/webAppConfig.java
@@ -2,11 +2,15 @@ package lk.ijse.notetaker.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.notetaker")
 @EnableWebMvc
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class webAppConfig {
 
 }
