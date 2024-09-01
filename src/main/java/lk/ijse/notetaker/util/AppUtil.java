784b3876a044ee6500d8f2415e6b29b256354e15
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-09-01 16:29:01 +0530

    Create util for Base64 Handling

--- src/main/java/lk/ijse/notetaker/util/AppUtil.java
+++ src/main/java/lk/ijse/notetaker/util/AppUtil.java
@@ -3,6 +3,7 @@ package lk.ijse.notetaker.util;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Base64;
 import java.util.UUID;
 
 public class AppUtil {
@@ -13,6 +14,10 @@ public class AppUtil {
 
     public static String createUserId(){
         return "USER-"+UUID.randomUUID();
+    }
+
+    public static String toBase64ProfilePic(String profilePic){//profile pic eka convert karanna tamai me code set eken wenne
+        return Base64.getEncoder().encodeToString(profilePic.getBytes());
     }
 
 }
