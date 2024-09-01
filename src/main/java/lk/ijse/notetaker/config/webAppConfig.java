0fedda6542e6f3aed78d0b2af4ef9de3eec6cf2a
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-09-01 16:08:26 +0530

    Add config in WebAppConfig for Multipart form data

--- src/main/java/lk/ijse/notetaker/config/webAppConfig.java
+++ src/main/java/lk/ijse/notetaker/config/webAppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.notetaker.config;
 
+import jakarta.servlet.annotation.MultipartConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableJpaRepositories(basePackages = "lk.ijse.notetaker")
 @EnableTransactionManagement
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 2,// yam kisi value ekak dammama ,file ekk upload karata passe tmp sve unama mekt wda ek value ekak awot gnne file system eka ,aduwen awoth primary memory eka
+        maxFileSize = 1024 * 1024 * 10,//upload krn file eke size eka tiranaya karaai(dan tiyenne 10mb)
+        maxRequestSize = 1024 * 1024 * 50 //upload krn file ekath athuluwa sampurana data wala ekathuwa tamai meken kiyanne
+)
 public class webAppConfig {
 
 }
